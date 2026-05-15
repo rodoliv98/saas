eba48c9657705ad468a94f08249ed5283f3c2c87
Rdg Oliv <rdgoliv18@gmail.com>
2026-05-15 16:30:37 -0300

    admin criado

--- back/src/api/admin/admin-routes.ts
+++ back/src/api/admin/admin-routes.ts
@@ -4,7 +4,6 @@ import { AdminRepository } from "./admin-repo";
 import { Router } from "express";
 import { checkAdmin } from "../../middlewares/check-admin";
 import prisma from '../../lib/client';
-import bcrypt from 'bcrypt';
 
 const router = Router();
 const repo = new AdminRepository(prisma);
@@ -18,23 +17,5 @@ router.get('/admin/tenants', checkAdmin, controller.findAllTenants.bind(controll
 router.patch('/admin/tenants/store', checkAdmin, controller.changeStoreStatus.bind(controller));
 
 router.patch('/admin/tenants/active', checkAdmin, controller.changeStoreActiveStatus.bind(controller));
-
-router.get('/test-acc', async (_req, res) => {
-  console.log('entrou');
-  const body = {
-    email: 'rodoliv@email.com',
-    senha: 'Qwe@794613'
-  }
-  const hash = await bcrypt.hash(body.senha, 10);
-
-  const admin = await prisma.admins.create({
-    data: {
-      email: body.email,
-      senha: hash
-    }
-  });
-  console.log('admin criado', admin);
-  res.status(200).json(admin);
-})
 
 export default router;
