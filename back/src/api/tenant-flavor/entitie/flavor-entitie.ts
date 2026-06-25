2dfa5c851d66a07ccdad49871664da621cb55b74
Rdg Oliv <rdgoliv18@gmail.com>
2026-06-24 21:41:33 -0300

    removido comentario em flavor-entities

--- back/src/api/tenant-flavor/entitie/flavor-entitie.ts
+++ back/src/api/tenant-flavor/entitie/flavor-entitie.ts
@@ -1,15 +1,3 @@
-import { Decimal } from "@prisma/client/runtime/library";
 import { Sabores } from "../../../generated/prisma/client";
 
-/* export interface Flavor {
-  tenantId: string;
-  id: string;
-  produtoId: string;
-  nomeProduto: string;
-  descProduto: string;
-  precoProduto: Decimal;
-  categoria: string;
-  imageUrl: string;
-}
- */
 export type Flavor = Sabores;
